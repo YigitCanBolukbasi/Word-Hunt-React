06b32efdf9d476ba02042523451034749972df38
Yigit Can <y.can.bolukbasi@gmail.com>
2022-01-28 15:08:31 +0300

    React Router Implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
+  );
+}
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+    </>
   );
 }
 
