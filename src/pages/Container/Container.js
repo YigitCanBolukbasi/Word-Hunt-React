c07058a056b5be1cb491eff7b956f1a14176e207
Yigit Can <y.can.bolukbasi@gmail.com>
2022-01-29 18:10:08 +0300

    refactor router

--- src/pages/Container/Container.js
+++ src/pages/Container/Container.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Login from "../Login/Login";
 import Register from "../Register/Register";
 import NavBar from "../../components/NavBar/NavBar";
+import Translate from "../Translate/Translate";
+import AuthContext from "../../context/AuthContext";
 
 function Container() {
+  const { loggedIn } = useContext(AuthContext);
   return (
     <div>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        {loggedIn ? (
+          <>
+            <Route path="/" element={<Translate />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </>
+        )}
       </Routes>
     </div>
   );
