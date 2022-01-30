64ee01e20137dd4ada75b3088cc6a8f4b3b7a10a
Yigit Can <y.can.bolukbasi@gmail.com>
2022-01-31 00:17:49 +0300

    Conditional Css Implemented

--- src/components/Modal/TestModal.js
+++ src/components/Modal/TestModal.js
@@ -6,6 +6,9 @@ function TestModal() {
   const [question, setQuestion] = useState("");
   const [questionID, setQuestionID] = useState("");
   const [words, setWords] = useState([]);
+  const [answerid, setAnswerid] = useState();
+  const [correctColor, setCorrectColor] = useState(false);
+  const [falseColor, setFalseColor] = useState(false);
 
   function getQuestion() {
     axiosInstance
@@ -23,6 +26,7 @@ function TestModal() {
           setWords(words_array);
           setQuestion(data.correct_word.en);
           setQuestionID(data.id);
+          setAnswerid(data.correct_word.id);
         }
       });
   }
@@ -33,14 +37,40 @@ function TestModal() {
         word_id: word_id,
       })
       .then((response) => {
-        console.log(response);
+        console.log("reposnsee", response);
         if (response.data.answer == "correct") {
           toast.success("Congrats!!");
+          const correctAnswer = document.getElementById(answerid);
+          setCorrectColor(true);
         } else {
           toast.warn("Upss!!");
+          setFalseColor(true);
         }
       });
   }
+
+  const modalClose = () => {
+    setFalseColor(false);
+    setCorrectColor(false);
+  };
+
+  const setColor = (word) => {
+    if (correctColor && answerid === word.id) {
+      return "btn btn-success w-100";
+    } else if (falseColor) {
+      if (answerid === word.id) {
+        return "btn btn-success w-100";
+      } else {
+        return "btn btn-danger w-100";
+      }
+    } else {
+      if (correctColor) {
+        return "btn btn-danger w-100";
+      } else {
+        return "btn btn-info w-100";
+      }
+    }
+  };
 
   return (
     <div>
@@ -51,8 +81,7 @@ function TestModal() {
           class="btn btn-primary"
           data-bs-toggle="modal"
           data-bs-target="#exampleModal"
-          onClick={getQuestion}
-        >
+          onClick={getQuestion}>
           Launch!
         </button>
       </div>
@@ -62,8 +91,7 @@ function TestModal() {
         id="exampleModal"
         tabindex="-1"
         aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
+        aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
@@ -74,8 +102,7 @@ function TestModal() {
                 type="button"
                 class="btn-close"
                 data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
+                aria-label="Close"></button>
             </div>
             <div class="modal-body">
               <h5>{question}</h5>
@@ -85,10 +112,10 @@ function TestModal() {
                     return (
                       <div className="col-md-12 mt-2 p-1" key={word?.id}>
                         <button
-                          class="btn btn-info w-100"
+                          id={word?.id}
+                          class={setColor(word)}
                           type="button"
-                          onClick={() => submitQuestion(word.id)}
-                        >
+                          onClick={() => submitQuestion(word.id)}>
                           {word?.tr}
                         </button>
                       </div>
@@ -102,7 +129,7 @@ function TestModal() {
                 type="button"
                 class="btn btn-secondary"
                 data-bs-dismiss="modal"
-              >
+                onClick={modalClose}>
                 Close
               </button>
             </div>
