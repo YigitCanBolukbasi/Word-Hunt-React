0b8c0443981ed2dfc0e0575ca5d750c96c7734a8
sezeristif <sezeristif@gmail.com>
2022-01-30 15:35:31 +0300

    update boostrap css with scss

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/js/bootstrap";
+import "./custom_bootstrap.scss";
 
 ReactDOM.render(
   <React.StrictMode>
