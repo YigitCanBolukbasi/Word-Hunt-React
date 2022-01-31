3b0c13bb0a081480e6be6ebf9e7640fa4a5d7899
Yigit Can <y.can.bolukbasi@gmail.com>
2022-01-31 13:33:31 +0300

    Userstats styling added

--- src/components/UserStats/UserStats.js
+++ src/components/UserStats/UserStats.js
@@ -8,10 +8,47 @@ const UserStats = () => {
   useEffect(() => {
     axiosInstance.get("/api/v1/users/me").then((response) => {
       setUserMetrics(response.data);
+      console.log(userMetrics);
     });
   }, []);
 
-  return <div>{userMetrics.correct_question_count}</div>;
+  return (
+    <div>
+      <div>
+        <div className="container">
+          <div className="row  my-5">
+            <div className="col-md-6 mx-auto border border-primary rounded p-5">
+              <table class="table table-striped">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">User Stats</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Correct question count</td>
+                    <td>{userMetrics.correct_question_count}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Learned word count</td>
+                    <td>{userMetrics.learned_word_count}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Wrong question count</td>
+                    <td>{userMetrics.wrong_question_count}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserStats;
